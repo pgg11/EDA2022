447335430a990561eb510a82622cdf9a0e02473a
Pablo <pablogg011@gmail.com>
2022-05-13 00:03:50 -0300

    Se agregó side al constructor de PawnBoard y diccionario con objetos Pawn, una funcion markBorders que marca los bordes del tablero para que los peones no se puedan mover más allá de ellos, una función que da posición inicial a los peones o busca su posicion y las guarda en el diccionario de Pawns. updatePawnBoard ahora tambien marca los cuadrados afectados por paredes. processMove ahora retorna el valor devuelto por una función de la clase Pawn según el lado que tocó en la partida(N o S)

--- board.py
+++ board.py
@@ -1,21 +1,80 @@
-
 import square
+import pawn
 
 class PawnBoard():
     
-    def __init__(self):
-        self.board = [[square.BoardSquare(row,col,"") for col in range(9)] for row in range(9)]
+    def __init__(self,side):
+        self.board = [[square.BoardSquare("") for col in range(9)] for row in range(9)]
+        self.side = side
+        self.pawns = {}
+
+
     
-    def updatePawnBoard(self,strBoard):
+    #Marca los bordes del tablero como lados bloqueados
+    def markBorders(self):
         for row in range(9):
+            for col in range(9):
+                if row==0:
+                    self.board[row][col].topBorder=False
+                if row==8:
+                    self.board[row][col].botBorder=False
+                if col==0:
+                    self.board[row][col].leftBorder=False
+                if col==8:
+                    self.board[row][col].rightBorder=False
+    
+    def positionPawns(self):
+        #Si no tengo ubicados los peones, los agrego a un diccionario con las posiciones iniciales
+        if len(self.pawns) == 0:
+            self.markBorders()
+            if self.side=="N":
+                self.pawns[0] = pawn.Pawn(0,1)
+                self.pawns[1] = pawn.Pawn(0,4)
+                self.pawns[2] = pawn.Pawn(0,7)
+            else:
+                self.pawns[0] = pawn.Pawn(8,1)
+                self.pawns[1] = pawn.Pawn(8,4)
+                self.pawns[2] = pawn.Pawn(8,7)
+        else:
+            pawnCounter = 0
+            for row in range(9):
+                for col in range(9):
+                    if self.side == 'N':
+                        if self.board[row][col].value == 'N':
+                            self.pawns[pawnCounter].posY = row
+                            self.pawns[pawnCounter].posX = col
+                            pawnCounter += 1
+                    else:
+                        if self.board[row][col].value == 'S':
+                            self.pawns[pawnCounter].posY = row
+                            self.pawns[pawnCounter].posX = col
+                            pawnCounter += 1
+
+    def updatePawnBoard(self,strBoard):
+        #Con un puntero marco el caracter strBoard para buscar paredes
+        boardPointer = 0
+        for row in range(9):
+            #Aisla fila del tablero en cada iteracion
             string_row=strBoard[17*(row*2): 17*(row*2) + 17]
             for col in range(9):
+                if strBoard[boardPointer+col+1] == '|' and col < 8:
+                    #Si encuentra una pared vertical marca los bordes afectados con False
+                    self.board[row][col-1].rightBorder=False
+                    self.board[row][col+1].leftBorder=False
+                if strBoard[boardPointer+col+9] == '-' and col < 8:
+                    #Si encuentra una pared horizontal marca los bordes afectados con False
+                        self.board[row][col].botBorder=False
+                        self.board[row+1][col].topBorder=False
+                #Revisa si hay un peon en el cuadrado
                 if string_row[col*2]=="N":
-                    self.board[row][col]=square.BoardSquare(row,col,"N")
+                    self.board[row][col]=square.BoardSquare("N")
                 elif  string_row[col*2]=="S":
-                    self.board[row][col]=square.BoardSquare(row,col,"S")
+                    self.board[row][col]=square.BoardSquare("S")
                 else:
-                    self.board[row][col]=square.BoardSquare(row,col," ")
+                    self.board[row][col]=square.BoardSquare(" ")
+            #Sumo 17 al puntero para pasar a la siguiente linea
+            if row<7:
+                boardPointer+=34
     
     def printPawnBoard(self):
         for row in range(9):
@@ -24,12 +83,9 @@ class PawnBoard():
             print()
     
     #Falta aplicar lógica de movimiento de peones
-    def processMove(self,side):
-        for row in range(9):
-            for col in range(9):
-                if self.board[row][col].value == side:
-                    print(self.board[row][col].value)
-                    if side == "N":
-                        return ({"from_row":row, "to_row":row+1, "from_col":col, "to_col":col })
-                    if side == "S":
-                        return ({"from_row":row, "to_row":row-1, "from_col":col, "to_col":col })
+    def processMove(self):
+        self.positionPawns()
+        if self.side == 'N':
+            return pawn.Pawn.northMoveForward(self.board,self.pawns)
+        else:
+            return pawn.Pawn.southMoveForward(self.board,self.pawns)
