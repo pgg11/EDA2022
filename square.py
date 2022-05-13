552be239dc400279e92ee8180484490b67e72ffc
Pablo <pablogg011@gmail.com>
2022-05-13 00:05:43 -0300

    Se quitaron las posiciones X e Y, se agregaron valores boolenos que hacen referencia si son posibles moviemientos a cada costado del cuadrado

--- square.py
+++ square.py
@@ -1,8 +1,11 @@
 
 class BoardSquare():
     
-    def __init__(self,row,col,value):
+    def __init__(self,value):
         self.value = value
-        self.posX = row
-        self.posY = col
-
+        #Se agregan valores booleanos a cada lado del cuadrado para
+        #saber si el peón se puede mover hacia ese lado
+        self.topBorder = True
+        self.rightBorder = True
+        self.botBorder = True
+        self.leftBorder = True
